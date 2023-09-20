c185cf482b44b8ce9338712f27142e3f8e7661f8
Abed Nganga <ngangaabed@gmail.com>
2023-09-20 23:57:36 +0300

    Scraping HTML with Beautiful Soup

--- lib/scraper.py
+++ lib/scraper.py
@@ -1,6 +1,11 @@
-from turtle import ht
+# from turtle import ht
 from bs4 import BeautifulSoup
 import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
-html = requests.get("https://flatironschool.com/", headers=headers)
+# html = requests.get("https://flatironschool.com/", headers=headers)
+
+html = requests.get("https://Moringaschool.com/", headers=headers)
+doc = BeautifulSoup(html.text, 'html.parser')
+
+print(doc.select('.grid.grid-cols-1.gap-6'))
